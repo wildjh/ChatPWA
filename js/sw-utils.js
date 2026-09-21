625710dd3d6c333a59a2118320642675880c08a1
wildjh <william.huertas01@uptc.edu.co>
2026-09-21 16:32:33 -0500

    Primera version

---
+++ js/sw-utils.js
@@ -0,0 +1,15 @@
+
+
+// Guardar  en el cache dinamico
+function actualizaCacheDinamico(dynamicCache,
+    req, res) {
+    if (res.ok || res.type === 'opaque') {
+        return
+        caches.open(dynamicCache).then(cache => {
+            cache.put(req, res.clone());
+            return res;
+        });
+    } else {
+        return res;
+    }
+}
